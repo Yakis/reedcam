11cb09c49aafbb952f29bd99006cb48d750e727c
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2022-03-11 10:47:08 +0000

    Initial Commit

---
+++ reedcam/reedcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  reedcamApp.swift
+//  reedcam
+//
+//  Created by Mugurel Moscaliuc on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct reedcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
